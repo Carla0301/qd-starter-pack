44f2feb3e6e17b279e623c76fed00148c965bc7e
Carla0301 <cmasca02@gmail.com>
2023-11-08 18:59:34 +0100

    feat: creazione random_number.py

---
+++ src/random_number.py
@@ -0,0 +1,11 @@
+
+#  Write a program that generates a random number.
+
+#  Output:
+#  The random number is: 4
+import random
+def funzione():
+    print("genero un numero random")
+    num=random.random
+    print(num)
+    
